ec5858d99f14b032d607a381158771f4870866ff
Sofia Ailin Battafarano <sofia-battafarano@MacBook-Air-de-Sofita.local>
2019-06-08 19:58:41 -0300

    cambios varios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,6 +15,7 @@ class UsersTableSeeder extends Seeder
         // Usuario con el rol administrador
 
         $administrador = User::create([
+            'codigo'  => 'Aco80',
             'name'     => 'Marcos Alejandro Bocchio' ,
             'email'    => 'marcosbocchio@gmail.com',
             'password' => bcrypt('12345678'),
@@ -26,6 +27,7 @@ class UsersTableSeeder extends Seeder
         // Usuario con el rol operador
 
         $operador = User::create([
+            'codigo'  => 'Ruso74',
             'name'     => 'Pablo Martin Bocchio' ,
             'email'    => 'pablobocchio@hotmail.com',
             'password' => bcrypt('12345678'),
@@ -36,6 +38,7 @@ class UsersTableSeeder extends Seeder
         // Usuario con el rol Cliente
 
         $cliente = User::create([
+            'codigo'  => 'Sofita',
             'name'     => 'Sofia Ailín Battafarano' ,
             'email'    => 'sofia_battafarano@hotmail.com',
             'password' => bcrypt('12345678'),
@@ -46,6 +49,38 @@ class UsersTableSeeder extends Seeder
         $cliente = User::create([
             'name'     => 'julian' ,
             'email'    => 'julian@hotmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $cliente->givePermissionTo('Navegar cliente');
+
+        $cliente = User::create([
+            'name'     => 'Karem' ,
+            'email'    => 'karem@hotmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $cliente->givePermissionTo('Navegar cliente');
+
+        $cliente = User::create([
+            'name'     => 'Lautaro' ,
+            'email'    => 'lautaro@hotmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $cliente->givePermissionTo('Navegar cliente');
+
+        $cliente = User::create([
+            'name'     => 'Oscar' ,
+            'email'    => 'oscar@hotmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $cliente->givePermissionTo('Navegar cliente');
+
+        $cliente = User::create([
+            'name'     => 'Marianella' ,
+            'email'    => 'Marianella@hotmail.com',
             'password' => bcrypt('12345678'),
         ]);
 
