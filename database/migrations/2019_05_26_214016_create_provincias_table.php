1d02d801faa4fc7da681b542144e932c8f6374a2
Marcos <marcosbocchio@gmail.com>
2019-07-11 00:51:43 +0000

    cambios varios

---
+++ database/migrations/2019_05_26_214016_create_provincias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProvinciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provincias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('provincia');
+            $table->bigInteger('pais_id')->unsigned();
+            $table->boolean('fav')->default(false);
+            $table->timestamps();
+            $table->foreign('pais_id')->references('id')->on('paises');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provincias');
+    }
+}
---
+++ database/migrations/2019_05_26_214028_create_localidades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocalidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localidades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('localidad');
+            $table->bigInteger('provincia_id')->unsigned();
+            $table->string('lat',100)->nullable();
+            $table->string('lon',100)->nullable();
+            $table->boolean('fav')->default(false);
+            $table->timestamps();
+            $table->foreign('provincia_id')->references('id')->on('provincias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('localidades');
+    }
+}
