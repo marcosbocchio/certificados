86e22f2347963924d9caf9a418e3cf2ac200e9a4
Marcos <marcosbocchio@gmail.com>
2019-09-02 19:21:34 +0000

    cambios varios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-      //  Schema::defaultStringLength(191);   
+        Schema::defaultStringLength(250);   
         
         view()->composer('*', function($view) {
             $view->with('documentos', Documentaciones::all());
