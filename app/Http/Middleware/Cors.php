7dc428b9fcc2f21647aad6dec692e37cfa143cc2
Sofia Ailin Battafarano <sofia-battafarano@MacBook-Air-de-Sofita.local>
2019-12-27 22:19:40 -0300

    cambios varios

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,13 +7,6 @@ class Cors {
 public function handle($request, Closure $next)
     {
 
-        return $next($request)
-              ->header("Access-Control-Allow-Origin", "*")
-              ->header("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT,DELETE")
-              ->header("Access-Control-Allow-Headers","Content-Type, X-Auth-Token, Origin, Authorization");
-
-                
-/*
         header("Access-Control-Allow-Origin: *");
         //ALLOW OPTIONS METHOD
         $headers = [
@@ -30,6 +23,6 @@ public function handle($request, Closure $next)
         }
         return $response;
         
-        */
+       
     }
 }
