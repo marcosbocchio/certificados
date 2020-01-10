38e1b8734008457cbda53da3b2482237e45cc8ec
Marcos <marcosbocchio@gmail.com>
2020-01-10 23:45:41 +0000

    cambios varios

--- resources/views/layouts/enod/master.blade.php
+++ resources/views/layouts/enod/master.blade.php
@@ -113,6 +113,15 @@ desired effect
   });
 });
 
+var url = window.location;
+// for sidebar menu but not for treeview submenu
+$('ul.sidebar-menu a').filter(function() {
+    return this.href == url;
+}).parent().siblings().removeClass('active').end().addClass('active');
+// for treeview which is like a submenu
+$('ul.treeview-menu a').filter(function() {
+    return this.href == url;
+}).parentsUntil(".sidebar-menu > .treeview-menu").siblings().removeClass('active menu-open').end().addClass('active menu-open');
 </script>
 </body>
 </html>
