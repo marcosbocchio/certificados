9dc11a04d601e17df5774a91be64a5bd58fc6507
Sofia Ailin Battafarano <sofia-battafarano@MacBook-Air-de-Sofita.local>
2020-01-28 19:43:26 -0300

    cambios varios

--- resources/views/layouts/enod/partials/header.blade.php
+++ resources/views/layouts/enod/partials/header.blade.php
@@ -8,7 +8,9 @@
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>A</b>LT</span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Enod</b>NDT</span>
+            <span class="logo-lg">
+                 <img src="{{asset('img/logo-enod.png')}}" style="margin-top: -10px;" height="70px" alt="Logo Enod"> 
+            </span>
         </a>
 
         <!-- Header Navbar -->
