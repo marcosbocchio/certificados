173b7b8c69538f21e9c758b19b1b8d411ff304b6
Sofia Ailin Battafarano <sofia-battafarano@MacBook-Air-de-Sofita.local>
2020-02-03 13:39:26 -0300

    cambios varios

--- resources/js/components/dashboard/dashboard-enod.vue
+++ resources/js/components/dashboard/dashboard-enod.vue
@@ -139,8 +139,8 @@
                                 <th class="col-lg-1">ESTADO</th>                        
                                 <th class="col-lg-1" colspan="4">
                                  <small style="margin-left: 2px;">Editar</small>
-                                 <small style="margin-left: 16px;">Usuario</small>
-                                 <small style="margin-left: 13spx;">Informe</small>
+                                 <small style="margin-left: 13px;">Usuario</small>
+                                 <small style="margin-left: 16px;">Informe</small>
                                  <small style="margin-left: 13px;">Estados</small>
 
                                 </th>
