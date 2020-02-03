6cfbcbde49a373eabc0555ddff019b8c8b43b056
Sofia Ailin Battafarano <sofia-battafarano@MacBook-Air-de-Sofita.local>
2020-02-03 10:50:53 -0300

    cambios varios

--- resources/js/components/dosimetria/dosimetria-estados.vue
+++ resources/js/components/dosimetria/dosimetria-estados.vue
@@ -202,7 +202,7 @@ export default {
 
         if(val) {
             let date = new Date(val);
-            return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
+            return ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()))  + '/' + ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + date.getFullYear();
         }else
         {
             return null;
