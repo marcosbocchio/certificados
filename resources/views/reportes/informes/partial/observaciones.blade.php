f043f40aef3c592208e7a006557fd00a75af76de
Marcos <marcosbocchio@gmail.com>
2020-07-08 02:29:54 +0000

    cambios varios

--- resources/views/reportes/informes/partial/observaciones.blade.php
+++ resources/views/reportes/informes/partial/observaciones.blade.php
@@ -4,7 +4,7 @@
             <td><strong style="font-size: 13px;">Observaciones</strong></td>
         </tr>   
         <tr>           
-            <td style="font-size: 13px; height:30px;" class="bordered-td">{{$observaciones}}</td> 
+            <td style="font-size: 13px; height:30px;" class="bordered-td"><span style="margin-left: 5px;">{{$observaciones}}</span></td> 
         </tr>
     </tbody>
 </table>
