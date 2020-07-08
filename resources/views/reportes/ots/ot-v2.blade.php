c545e618f60949d5dd2268357dce9ff462309ea4
Marcos <marcosbocchio@gmail.com>
2020-07-08 02:38:04 +0000

    cambios varios

--- resources/views/reportes/ots/ot-v2.blade.php
+++ resources/views/reportes/ots/ot-v2.blade.php
@@ -77,18 +77,19 @@ footer {
     <table width="100%">
         <tbody>
             <tr>
-                <td style="font-size: 13px;" colspan="6"><b>Firmas </b></td> 
+                <td style="font-size: 13px;" colspan="4"><b>Firmas </b></td> 
             </tr>
             <tr>
-                <td style="font-size: 13px;text-align: center;height: 50px;" colspan="2">
+                <td style="font-size: 13px;text-align: center;height: 50px;" colspan="2" width="50%">
                     @if($evaluador && $evaluador->path)
                         <img src="{{ public_path($evaluador->path)}}" alt="" style="width: 100px;height: 50px;">
                     @endif     
-                </td>               
+                </td>    
+                <td style="font-size: 13px;" colspan="2" width="50%">&nbsp;</td> 
             </tr>
             <tr>                               
-                <td style="font-size: 14px; text-align: center;"" colspan="3"><em>Responsable OT </em></td>   
-                <td style="font-size: 14px; text-align: center;" colspan="3"><em>Generador OT </em></td> 
+                <td style="font-size: 14px; text-align: center;"" colspan="2"><em>Responsable OT </em></td>   
+                <td style="font-size: 14px; text-align: center;" colspan="2"><em>Generador OT </em></td> 
             </tr>
         </tbody>
     </table>
