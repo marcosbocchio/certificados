741c469e3f7d3c926756c1b88b0b99c2c1f427d9
Marcos <marcosbocchio@gmail.com>
2020-07-26 01:42:48 +0000

    cambios varios

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,12 +8,18 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-      return $next($request)
-         //Url a la que se le dará acceso en las peticiones
-        ->header("Access-Control-Allow-Origin", '*')
-        //Métodos que a los que se da acceso
-        ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-        //Headers de la petición
-        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
+        header("Access-Control-Allow-Origin: *");
+             $headers = [
+                 'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS' ,
+                 'Access-Control-Allow-Headers' => ' Origin, Content-Type, Authorization, X-Auth-Token'
+             ] ;
+             if ( $request->getMethod() == "OPTIONS" ) {
+                 return response()->json('OK', 200, $headers );
+             }
+             $response = $next( $request ) ;
+             foreach ( $headers as $key => $value ) {
+                 $response->header( $key, $value ) ;
+             }
+             return $response ;
     }
 }
