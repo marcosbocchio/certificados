df6436b4a1978627561948775dd2ae916557f682
Marcos <marcosbocchio@gmail.com>
2020-09-01 19:36:01 +0000

    cambios varios

--- resources/views/reportes/partial/header-proyecto-portrait.blade.php
+++ resources/views/reportes/partial/header-proyecto-portrait.blade.php
@@ -1,19 +1,19 @@
 <table width="100%" style="margin-top:10px">
-    <tbody>             
-        <tr>                                                  
-            <td width="50%" style="font-size: 12px;"><b>Proyecto: </b></td>  
-            <td width="25%%" style="font-size: 12px;"><b>Obra </b></td> 
-            <td width="25%%" style="font-size: 12px;"><b>OT N° </b></td>     
-        </tr>   
+    <tbody>
+        <tr>
+            <td width="50%" style="font-size: 12px;"><b>Proyecto </b></td>
+            <td width="25%%" style="font-size: 12px;"><b>Obra </b></td>
+            <td width="25%%" style="font-size: 12px;"><b>OT N° </b></td>
+        </tr>
         <tr>
             <td style="font-size: 12px;"><span class="datosHead">{{$ot->proyecto}}</span></td>
-            
+
             @if(isset($informe))
-                <td style="font-size: 12px;" ><span class="datosHead">{{$informe->obra}}</span></td>     
+                <td style="font-size: 12px;" ><span class="datosHead">{{$informe->obra}}</span></td>
             @else
                 <td style="font-size: 12px;"><span class="datosHead"></span>{{$ot->obra}}</td>
-            @endif   
+            @endif
             <td style="font-size: 12px;"><span class="datosHead"><span class="datosHead"></span>{{$ot->numero}}</td>
         </tr>
     </tbody>
-</table>  
+</table>
--- resources/views/reportes/partial/nro_pagina.blade.php
+++ resources/views/reportes/partial/nro_pagina.blade.php
@@ -3,7 +3,7 @@
     if ( isset($pdf) ) {
         $x = 457;
         $y = 66;
-        $text = "PÁGINA : {PAGE_NUM} de {PAGE_COUNT}";
+        $text = "PAGINA : {PAGE_NUM} de {PAGE_COUNT}";
         $font = $fontMetrics->get_font("serif", "bold");
         $size = 9;
         $color = array(0,0,0);
@@ -13,7 +13,7 @@
         $pdf->page_text($x, $y, $text, $font, $size, $color, $word_space, $char_space, $angle);
 
         /* $pdf->line(34,167,561,167,array(0,0,0),1.5); */
-        
+
     }
 
 </script>
