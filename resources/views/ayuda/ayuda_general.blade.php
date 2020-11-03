8670be64fef8b72b0873920af46f680b1e9e3358
Marcos <marcosbocchio@gmail.com>
2020-11-03 18:14:12 +0000

    cambios varios

--- resources/views/ayuda/ayuda_general.blade.php
+++ resources/views/ayuda/ayuda_general.blade.php
@@ -15,40 +15,41 @@
 		</div>
         <div class="col-sm-12">
           <h3>Tablero Principal&nbsp;</h3>
+
           @can('enod')
               <p><a href="{{ route('ayuda-crear-ot') }}">Cómo crear una Orden de trabajo (OT)&nbsp;</a></p>
               <p><a href="{{ route('ayuda-asignar-soldadores-y-usuarios') }}">Asignar soldadores y usuarios de clientes a Orden de trabajo (OT)&nbsp;</a></p>
               <p><a href="{{ route('ayuda-asignar-operadores') }}" target="_blank">Asignar operadores a una Orden de trabajo (OT)&nbsp;</a></p>
-          @endcan
+           @can
 
               <p><a href="{{ route('ayuda-visualizar-doc-operadores') }}" target="_blank">Visualizar documentación de operadores asignados a una Orden de trabajo (OT)&nbsp;</a></p>
 
           @can('enod')
               <p><a href="{{ route('ayuda-asignar-procedimientos') }}" target="_blank">Asignar procedimientos a la Orden de trabajo (OT)&nbsp;</a></p>
-          @endcan
+           @can
 
           @can('enod')
               <p><a href="{{ route('ayuda-asignar-vehiculos') }}" target="_blank">Asignar vehículos y documentación complementaria a Orden de trabajo (OT)&nbsp;</a></p>
-          @endcan
+           @can
 
 			<p><a href="{{ route('ayuda-visualizar-vehiculos') }}" target="_blank"> Visualizar documentación de vehículos y documentación complementaria asignados a Orden de trabajo (OT)&nbsp;</a></p>
 
           @can('enod')
                 <p><a href="{{ route('error-404') }}" target="_blank">Creación de remitos&nbsp;</a></p>
                 <p><a href="{{ route('ayuda-generar-informes') }}"> Creación de informes&nbsp;</a></p>
-          @endcan
+           @can
 
                <p><a href="{{ route('error-404') }}" target="_blank"> Visualización de informes&nbsp;</a></p>
 
           @can('enod')
 			    <p><a href="{{ route('error-404') }}" target="_blank"> Creación de partes diarios&nbsp;</a></p>
-          @endcan
+           @can
 
 		    	<p><a href="{{ route('error-404') }}" target="_blank"> Visualización de partes diarios&nbsp;</a></p>
 
           @can('enod')
                <p><a href="{{ route('error-404') }}" target="_blank"> Creación de certificados&nbsp;</a></p>
-          @endcan
+           @can
             <a name="-maestros" />
 			<p><a href="{{ route('error-404') }}" target="_blank"> Visualización de certificados&nbsp;</a></p>
         </div>
@@ -75,7 +76,7 @@
                 <p><a href="{{ route('error-404') }}" target="_blank">Gestionar interno fuente&nbsp;</a></p>
                 <p><a href="{{ route('error-404') }}" target="_blank">Gestionar roles&nbsp;</a></p>
             </div>
-        @endcan
+         @can
 
 		<div class="col-sm-12">
           <h3>Dosimetría&nbsp;</h3>
@@ -84,18 +85,18 @@
                 <p><a href="{{ route('error-404') }}" target="_blank">Como registrar dosimetría operador&nbsp;</a></p>
                 <p><a href="{{ route('error-404') }}" target="_blank">Como registrar dosimetría RX&nbsp;</a></p>
                 <p><a href="{{ route('error-404') }}" target="_blank">Gestionar estados film&nbsp;</a></p>
-            @endcan
+             @can
             <p><a href="{{ route('error-404') }}" target="_blank">Visualizar resumen dosimetría&nbsp;</a></p>
             @can('enod')
               <p><a href="{{ route('error-404') }}" target="_blank">Visualizar reporte de altas y baja de film&nbsp;</a></p>
-            @endcan
+             @can
         </div>
 
 		<div class="col-md-12">
           <h3>Multimedia&nbsp;</h3>
           @can('enod')
               <p><a href="{{ route('error-404') }}" target="_blank">Gestionar contenido&nbsp;</a></p>
-          @endcan
+           @can
 			<p><a href="{{ route('error-404') }}" target="_blank">Visualizar contenido&nbsp;</a></p>
         </div>
 
--- resources/views/ayuda/visualizar_vehiculos.blade.php
+++ resources/views/ayuda/visualizar_vehiculos.blade.php
@@ -7,9 +7,9 @@
         <div class="col-sm-12">
             <h2>Visualizar documentación de vehículos y documentación complementaria asignados a Orden de trabajo (OT) </h2>
             <p>Un usuario con acceso al sistema, tiene la posibilidad de visualizar e incluso bajar la documentación asociada a todos los vehículos asignados a la OT. Además podrá visualizar y bajar información complementaria asociada a la OT.<br> A continuación, se muestra un ejemplo de visualización de documentación: </p>
-           </div>
+        </div>
 
-           <div class="col-sm-8 col-sm-offset-2">
+        <div class="col-sm-8 col-sm-offset-2">
             <img  class="img-responsive" src="{{ asset('img/ayuda/Visualizar_vehiculo.gif') }}" />
         </div>
     </div>
