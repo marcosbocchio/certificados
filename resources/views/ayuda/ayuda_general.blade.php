ceac7fbaa6d707099afcc529e0844b16cd8a53cf
Marcos <marcosbocchio@gmail.com>
2020-11-03 21:11:52 +0000

    cambios varios

--- resources/views/ayuda/ayuda_general.blade.php
+++ resources/views/ayuda/ayuda_general.blade.php
@@ -20,36 +20,36 @@
               <p><a href="{{ route('ayuda-crear-ot') }}">Cómo crear una Orden de trabajo (OT)&nbsp;</a></p>
               <p><a href="{{ route('ayuda-asignar-soldadores-y-usuarios') }}">Asignar soldadores y usuarios de clientes a Orden de trabajo (OT)&nbsp;</a></p>
               <p><a href="{{ route('ayuda-asignar-operadores') }}" target="_blank">Asignar operadores a una Orden de trabajo (OT)&nbsp;</a></p>
-           @can
+           @endcan
 
               <p><a href="{{ route('ayuda-visualizar-doc-operadores') }}" target="_blank">Visualizar documentación de operadores asignados a una Orden de trabajo (OT)&nbsp;</a></p>
 
           @can('enod')
               <p><a href="{{ route('ayuda-asignar-procedimientos') }}" target="_blank">Asignar procedimientos a la Orden de trabajo (OT)&nbsp;</a></p>
-           @can
+          @endcan
 
           @can('enod')
               <p><a href="{{ route('ayuda-asignar-vehiculos') }}" target="_blank">Asignar vehículos y documentación complementaria a Orden de trabajo (OT)&nbsp;</a></p>
-           @can
+          @endcan
 
 			<p><a href="{{ route('ayuda-visualizar-vehiculos') }}" target="_blank"> Visualizar documentación de vehículos y documentación complementaria asignados a Orden de trabajo (OT)&nbsp;</a></p>
 
           @can('enod')
                 <p><a href="{{ route('error-404') }}" target="_blank">Creación de remitos&nbsp;</a></p>
                 <p><a href="{{ route('ayuda-generar-informes') }}"> Creación de informes&nbsp;</a></p>
-           @can
+           @endcan
 
                <p><a href="{{ route('error-404') }}" target="_blank"> Visualización de informes&nbsp;</a></p>
 
-          @can('enod')
+           @can('enod')
 			    <p><a href="{{ route('error-404') }}" target="_blank"> Creación de partes diarios&nbsp;</a></p>
-           @can
+           @endcan
 
 		    	<p><a href="{{ route('error-404') }}" target="_blank"> Visualización de partes diarios&nbsp;</a></p>
 
           @can('enod')
                <p><a href="{{ route('error-404') }}" target="_blank"> Creación de certificados&nbsp;</a></p>
-           @can
+           @endcan
             <a name="-maestros" />
 			<p><a href="{{ route('error-404') }}" target="_blank"> Visualización de certificados&nbsp;</a></p>
         </div>
@@ -76,7 +76,7 @@
                 <p><a href="{{ route('error-404') }}" target="_blank">Gestionar interno fuente&nbsp;</a></p>
                 <p><a href="{{ route('error-404') }}" target="_blank">Gestionar roles&nbsp;</a></p>
             </div>
-         @can
+         @endcan
 
 		<div class="col-sm-12">
           <h3>Dosimetría&nbsp;</h3>
@@ -85,18 +85,18 @@
                 <p><a href="{{ route('error-404') }}" target="_blank">Como registrar dosimetría operador&nbsp;</a></p>
                 <p><a href="{{ route('error-404') }}" target="_blank">Como registrar dosimetría RX&nbsp;</a></p>
                 <p><a href="{{ route('error-404') }}" target="_blank">Gestionar estados film&nbsp;</a></p>
-             @can
+             @endcan
             <p><a href="{{ route('error-404') }}" target="_blank">Visualizar resumen dosimetría&nbsp;</a></p>
             @can('enod')
               <p><a href="{{ route('error-404') }}" target="_blank">Visualizar reporte de altas y baja de film&nbsp;</a></p>
-             @can
+             @endcan
         </div>
 
 		<div class="col-md-12">
           <h3>Multimedia&nbsp;</h3>
           @can('enod')
               <p><a href="{{ route('error-404') }}" target="_blank">Gestionar contenido&nbsp;</a></p>
-           @can
+           @endcan
 			<p><a href="{{ route('error-404') }}" target="_blank">Visualizar contenido&nbsp;</a></p>
         </div>
 
