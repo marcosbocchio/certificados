6d4203e7cb4effa582fdaf399b69c5267c641a03
Marcos <marcosbocchio@gmail.com>
2020-12-06 20:37:53 +0000

    cambios varios

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
      //  $schedule->command('command:task_date')->everyMinute();
-       $schedule->command('command:VencimientosDocumentaciones')->dailyAt('17:35');
+       $schedule->command('command:VencimientosDocumentaciones')->dailyAt('17:45');
        $schedule->command('command:DemoraCargaDosimetria')->dailyAt('03:05');    }
 
     /**
