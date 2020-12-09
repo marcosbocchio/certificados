d67b21ed044bca1232c42e4f24715a7dbdccc15a
Marcos <marcosbocchio@gmail.com>
2020-12-09 01:43:02 +0000

    cambios varios

--- app/Http/Controllers/NotificacionesController.php
+++ app/Http/Controllers/NotificacionesController.php
@@ -29,16 +29,6 @@ class NotificacionesController extends Controller
 
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
     public function getNotificaciones($user_id){
 
         return Notificaciones::where('user_id',$user_id)
@@ -57,22 +47,7 @@ class NotificacionesController extends Controller
 
 
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store($user_id,$item)
     {
         DB::enableQueryLog();
@@ -143,46 +118,7 @@ class NotificacionesController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $material = Notificaciones::find($id);
