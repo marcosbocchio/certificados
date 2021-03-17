555502dd30bd1326a339935e640a1608fbc21d10
marcosbocchio <marcosbocchio@gmail.com>
2021-03-17 13:11:25 -0300

    Se realizó cambio para permitir el mismo  fst en distintas Ots
    se exporto la imagen del logo enod para el reporte a un archivo.

--- app/Http/Requests/OtsRequest.php
+++ app/Http/Requests/OtsRequest.php
@@ -22,51 +22,51 @@ class OtsRequest extends FormRequest
      * @return array
      */
     public function rules()
-    {       
-             
+    {
+
          return [
             'ot'                       => 'required|digits_between:1,8|integer|unique:ots,numero,'. $this->id,
-            'fst'                      => 'required|digits_between:1,8|integer|unique:ots,presupuesto,'. $this->id,
+            'fst'                      => 'required|digits_between:1,8|integer',
             'cliente'                  => 'required',
             'proyecto'                 => 'required|Max:60',
             'fecha'                    => 'required',
             'hora'                     => 'required',
-            'obra'                     => 'Min:1|Max:8 |nullable', 
-            'contacto1'                => 'required', 
+            'obra'                     => 'Min:1|Max:8 |nullable',
+            'contacto1'                => 'required',
             'user_empresa'             => 'required',
             'provincia'                => 'required',
             'localidad'                => 'required',
             'fecha_ensayo'             => 'required',
             'lugar_ensayo'             => 'required',
             'lat'                      => 'required',
-            'lon'                      => 'required',          
-            'observaciones'            => 'Max:255',         
-            'servicios.*.cantidad_servicios' => 'required|integer|Min:1',          
+            'lon'                      => 'required',
+            'observaciones'            => 'Max:255',
+            'servicios.*.cantidad_servicios' => 'required|integer|Min:1',
             'servicios.*.cantidad_placas'    => 'nullable|integer|Min:1',
-            'productos.*.cantidad_productos'  =>'required|integer|Min:1',   
+            'productos.*.cantidad_productos'  =>'required|integer|Min:1',
             'productos.*.medida'              =>'required',
-                
-        ];     
-      
+
+        ];
+
     }
 
     public function attributes()
     {
     return [
         'ot'                                  => 'OT N°',
-        'fst'                                 => 'FST N°',  
-        'contacto1'                           => 'contacto 1',       
+        'fst'                                 => 'FST N°',
+        'contacto1'                           => 'contacto 1',
         'fecha_ensayo'                        => 'fecha estimada de ensayo',
         'lugar_ensayo'                        => 'lugar de ensayo',
-        'servicios.*.cantidad_servicios'      => 'cant.',  
-        'servicios.*.cantidad_placas'         => 'Max N° Placas', 
-        'productos.*.medida'                  => 'medida',   
+        'servicios.*.cantidad_servicios'      => 'cant.',
+        'servicios.*.cantidad_placas'         => 'Max N° Placas',
+        'productos.*.medida'                  => 'medida',
         'productos.*.cantidad_productos'      => 'cant.',
-        'user_empresa'                        => 'Responsable OT'    
+        'user_empresa'                        => 'Responsable OT'
 
          ];
     }
 
-  
+
 
 }
