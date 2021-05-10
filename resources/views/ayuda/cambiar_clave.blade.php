35eb2811e07751b8500ddff4731ab18f42374e04
marcosbocchio <marcosbocchio@gmail.com>
2021-05-09 23:09:30 -0300

    actualización de rutas

--- resources/views/ayuda/cambiar_clave.blade.php
+++ resources/views/ayuda/cambiar_clave.blade.php
@@ -6,9 +6,9 @@
        <div class="row">
         <div class="col-md-12">
             <h1>Cómo cambiar o restablecer la contraseña de tu cuenta</h1>
-            <p>Para iniciar sesión en tu cuenta debes usar el <strong>correo</strong> y la <strong>contraseña</strong>&nbsp;con la que estas registrado en la aplicacion de&nbsp;&nbsp;<a href="http://desarrollorusoft.com.ar/" target="_blank" ><strong>Enod</strong></a>. Esta información la puedes cambiar en cualquier momento.</p>
+            <p>Para iniciar sesión en tu cuenta debes usar el <strong>correo</strong> y la <strong>contraseña</strong>&nbsp;con la que estas registrado en la aplicacion de&nbsp;&nbsp;<a href="http://sigoenod.com/" target="_blank" ><strong>Enod</strong></a>. Esta información la puedes cambiar en cualquier momento.</p>
             <h2>Cambia la contraseña de tu cuenta</h2>
-            <p>Si deseas cambiar la contraseña de tu cuenta de <a href="http://desarrollorusoft.com.ar" target="_blank"><strong>Enod</strong>&nbsp;</a>, sigue estos pasos:</p>
+            <p>Si deseas cambiar la contraseña de tu cuenta de <a href="http://sigoenod.com" target="_blank"><strong>Enod</strong>&nbsp;</a>, sigue estos pasos:</p>
             <ol>
               <li>Dirígete al nombre de tu usuario en la parte superior derecha y haz click</li>
               <li>Haz click en <strong>Perfil</strong></li>
@@ -31,7 +31,7 @@
             <h2>¿Olvidate tu contraseña ?&nbsp;</h2>
             <p>Si olvidaste tu contraseña no te preocupes, puedes restablecerla siguiendo estos pasos:</p>
             <ol>
-              <li>Ingresa a <a href="http://desarrollorusoft.com.ar" target="_blank"><strong>Enod</strong> </a></li>
+              <li>Ingresa a <a href="http://sigoenod.com" target="_blank"><strong>Enod</strong> </a></li>
               <li>Haz Click en <strong>¿Olvidó su contraseña?</strong></li>
               <li>Introduzca su correo y has click en <strong>Restablecer</strong></li>
               <li>Le enviaremos un correo con el link y las instrucciones para restrablecer su contraseña</li>
