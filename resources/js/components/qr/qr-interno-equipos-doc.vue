e3cc6461b0654f3b5204d8c6566a12cfb2960799
marcosbocchio <marcosbocchio@gmail.com>
2022-08-30 14:24:39 -0300

    actualizacion

--- resources/js/components/qr/qr-interno-equipos-doc.vue
+++ resources/js/components/qr/qr-interno-equipos-doc.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="container-fluid">
+    <div>
+        <div class="container-fluid" v-show="!isLoading">
             <template v-if="documentaciones.length" class="vcenter">
                 <h5 style="margin-top:20px"><strong>N° Int {{documentaciones[0].nro_interno}} / N° Serie {{documentaciones[0].nro_serie}} </strong></h5>
                 <ul>
                     <li v-for="item in documentaciones" :key="item.id" style="margin-top:15px; margin">
         
-                        <a :href="'/' + item.path " target="_blank"> {{ item.titulo }} / {{ item.descripcion }}</a>
+                        <a :href="'/' + item.path " target="_blank" title="documentación"> {{ item.titulo }} / {{ item.descripcion }}</a>
                         
                     </li>   
                 </ul>
@@ -13,12 +14,12 @@
             <template v-else>
                 <h5>No se encontro documentacion para el equipo </h5>               
             </template>
-
-         <loading
+        </div>
+        <loading
             :active.sync="isLoading"
             :loader="'bars'"
             :color="'red'">
-         </loading>        
+        </loading>   
     </div>
 </template>
 
@@ -61,11 +62,14 @@ export default {
             this.documentaciones = [];
             axios.defaults.baseURL = this.url ;
             var urlRegistros = 'interno_equipos/' + this.interno_equipo_id + '/documentaciones/' + '?api_token=' + Laravel.user.api_token;
-            await axios.get(urlRegistros)
-            .then(response =>{
+            axios.get(urlRegistros).then(response =>{
                this.documentaciones = response.data
                console.log('data',response)
-            }).finally(this.$store.commit('loading', false));            
+               this.$store.commit('loading', false)
+            }).catch(function (error) {
+                console.log(error.toJSON());
+                this.$store.commit('loading', false)
+            });    
         }
     } 
 }
