8ae1db28974ad27d02325ed6f69225fe2a8ea645
lautarobocchio <lautarobocchio9@gmail.com>
2023-02-02 10:28:08 -0300

    firmas mas grandes. Ducto y Certificados

--- resources/views/reportes/informes/partial/firmas.blade.php
+++ resources/views/reportes/informes/partial/firmas.blade.php
@@ -1,19 +1,17 @@
 <table width="100%">
     <tbody>
         <tr>
-            <td style="font-size: 13px;" colspan="6"><b>Firmas </b></td>
-        </tr>
-        <tr>
-            <td style="font-size: 13px;text-align: center;height: 50px;" colspan="2" width="33.33%">
+            <td style="font-size: 13px;" colspan="1"  rowspan="2"><b>Firmas </b></td>
+            <td style="font-size: 13px;text-align: center;height: 90px;" colspan="2" width="33.33%">
                 @if($firma)
-                    <img src="{{ public_path($firma) }}" alt="" style="width: 100px;height: 50px;">
+                    <img src="{{ public_path($firma) }}" alt="" style="width: 180px;height: 90px;">
                 @endif
             </td>
             <td style="font-size: 13px;" colspan="2"  width="33.33%">&nbsp;</td>
             <td style="font-size: 13px;" colspan="2" width="33.33%">&nbsp;</td>
         </tr>
         <tr>
-            <td style="font-size: 14px; text-align: center;"" colspan="2"><em>Evaluador </em></td>
+            <td style="font-size: 14px; text-align: center;" colspan="2"><em>Evaluador </em></td>
             <td style="font-size: 14px; text-align: center;" colspan="2"><em>Cliente </em></td>
             <td style="font-size: 14px; text-align: center;" colspan="2"><em>Comitente </em></td>
         </tr>
