35df049ac065e970ffe8858d92efcd8a2e2a4ca4
SergioWeb98 <webser98quinteros@gmail.com>
2023-10-02 08:18:00 -0300

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,8 @@ class Kernel extends ConsoleKernel
                      //->monthly()
                      //->at('02:00');
             $schedule->command('command:generarZip')
-                     ->everyFiveMinutes();
+                    ->everyFifteenMinutes();
+
             $schedule->command('command:VencimientosDocumentaciones')->dailyAt(config('cron.time_cron_documentaciones'));
             $schedule->command('command:DemoraCargaDosimetria')->dailyAt(config('cron.time_cron_dosimetria'));
             
