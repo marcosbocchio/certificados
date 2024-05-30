e41e9df676ceb1e47ba4db040526a5c0c07c801c
SergioWeb98 <webser98quinteros@gmail.com>
2024-05-30 13:06:56 -0300

    ajuste_horas_extras

--- resources/js/components/control-asistencia/asistencia-resumen.vue
+++ resources/js/components/control-asistencia/asistencia-resumen.vue
@@ -64,7 +64,8 @@
             <td v-else>{{ operador.domingos }}</td>
             <td v-if="frente_selected.id === 2 && operador.operador.local_neuquen_sn === 0">-</td>
             <td v-else>{{ operador.feriados }}</td>
-            <td>{{ operador.horasExtras }}</td>
+            <td v-if="frente_selected.id === 2">-</td>
+            <td v-else>{{ operador.horasExtras }}</td>
             <td>{{ operador.serviciosExtrasS1 }}</td>
             <td><input type="checkbox" v-model="operador.pagoS1" :disabled="operador.precargadoPagoS1" /></td>
             <td>{{ operador.serviciosExtrasS2 }}</td>
