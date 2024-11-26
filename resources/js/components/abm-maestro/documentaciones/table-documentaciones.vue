103c17512e6f46cac7497e45a5aea3335ace915c
SergioWeb98 <webser98quinteros@gmail.com>
2024-11-26 08:59:08 -0300

    temp_archivos

--- resources/js/components/abm-maestro/documentaciones/table-documentaciones.vue
+++ resources/js/components/abm-maestro/documentaciones/table-documentaciones.vue
@@ -11,40 +11,42 @@
             <tr>
               <th><input type="checkbox" @change="toggleAll($event)" /></th>
               <th class="col-md-1">Tipo</th>
+              <th class="col-md-2">Título</th>
               <th class="col-md-4">Descripción</th>
               <th class="col-md-1">Método</th>
               <th class="col-md-2">Usuario</th>
               <th class="col-md-1">INT. Nº</th>
-              <th class="col-md-2">Fecha De Caducidad</th>
-              <th>&nbsp;</th>
+              <th >&nbsp;</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="registro in registros" :key="registro.id">
               <td><input type="checkbox" :value="registro" v-model="selectedRegistros" /></td>
-              <td>{{ registro.tipo }}</td>
-              <td>{{ registro.descripcion }}</td>
-              <td v-if="registro.metodo_ensayo.id">{{ registro.metodo_ensayo['metodo'] }}</td>
-              <td v-else-if="registro.interno_equipo.length > 0">{{ registro.interno_equipo[0].equipo.metodo_ensayos['metodo'] }}</td>
-              <td v-else>&nbsp;</td>
-              <td v-if="registro.usuario[0]">{{ registro.usuario[0]['name'] }}</td>
-              <td v-else-if="registro.tipo == 'EQUIPO' && registro.user_interno_equipo[0]">{{ registro.user_interno_equipo[0]['name'] }}</td>
-              <td v-else>&nbsp;</td>
-              <td v-if="registro.tipo == 'EQUIPO' && registro.interno_equipo[0]">{{ registro.interno_equipo[0]['nro_interno'] }}</td>
-              <td v-else-if="registro.tipo == 'FUENTE' && registro.interno_fuente[0]">{{ registro.interno_fuente[0]['nro_serie'] }}</td>
-              <td v-else-if="registro.tipo == 'VEHICULO' && registro.vehiculo[0]">{{ registro.vehiculo[0]['nro_interno'] }}</td>
-              <td v-else>&nbsp;</td>
-              <td>{{ formatFecha(registro.fecha_caducidad) }}</td>
-              <td width="10px">
-                <button class="btn btn-warning btn-sm" title="Editar" @click.prevent="$emit('editRegistroEvent', registro)" :disabled="!$can('M_documentaciones_edita')">
-                  <span class="fa fa-edit"></span>
-                </button>
-              </td>
-              <td width="10px">
-                <button class="btn btn-danger btn-sm" title="Eliminar" @click.prevent="$emit('confirmarDelete', registro, registro.titulo)" :disabled="!$can('M_documentaciones_edita')">
-                  <span class="fa fa-trash"></span>
-                </button>
-              </td>
+              <td v-if="registro.tipo == 'USUARIO'">USUARIOS</td>          
+            <td v-if="registro.tipo == 'OT'" >OT</td>          
+            <td v-if="registro.tipo == 'INSTITUCIONAL'">INSTITUCIONAL</td>    
+            <td v-if="registro.tipo == 'PROCEDIMIENTO GENERAL'">PROCEDIMIENTO GENERAL</td>    
+            <td v-if="registro.tipo == 'EQUIPO'">EQUIPO</td>
+            <td v-if="registro.tipo == 'FUENTE'">FUENTE</td>
+            <td v-if="registro.tipo == 'VEHICULO'">VEHICULO</td>   
+            <td>{{ registro.titulo}}</td>
+            <td>{{ registro.descripcion }}</td>
+            <td v-if="registro.metodo_ensayo.id">{{ registro.metodo_ensayo['metodo']}}</td>
+            <td v-else-if="registro.interno_equipo.length > 0">{{ registro.interno_equipo[0].equipo.metodo_ensayos['metodo']}}</td>
+            <td v-else>&nbsp;</td>
+            <td v-if="registro.usuario[0]">{{ registro.usuario[0]['name']}}</td>
+            <td v-else-if="registro.tipo == 'EQUIPO' && registro.user_interno_equipo[0]">{{registro.user_interno_equipo[0]['name'] }}</td>
+            <td  v-else>&nbsp;</td>
+            <td v-if="registro.tipo == 'EQUIPO' && registro.interno_equipo[0]">{{ registro.interno_equipo[0]['nro_interno']}}</td>
+            <td v-else-if="registro.tipo == 'FUENTE' && registro.interno_fuente[0]">{{ registro.interno_fuente[0]['nro_serie']}}</td>
+            <td  v-else-if="registro.tipo == 'VEHICULO' && registro.vehiculo[0]">{{ registro.vehiculo[0]['nro_interno']}}</td>
+            <td  v-else>&nbsp;</td>
+            <td width="10px">
+              <button class="btn btn-warning btn-sm" title="Editar" v-on:click.prevent="$emit('editRegistroEvent',registro)" :disabled="!$can('M_documentaciones_edita')"><span class="fa fa-edit"></span></button>
+            </td>
+            <td width="10px">
+              <button class="btn btn-danger btn-sm" title="Eliminar" v-on:click.prevent="$emit('confirmarDelete',registro,registro.titulo)" :disabled="!$can('M_documentaciones_edita')"><span class="fa fa-trash"></span></button>
+            </td>
             </tr>
           </tbody>
         </table>
