8fb8820ae7a369a2571f456a30a90c0840ad484e
SergioWeb98 <webser98quinteros@gmail.com>
2024-11-27 14:41:19 -0300

    test_carga

--- resources/js/components/control-asistencia/asistencia-nuevo.vue
+++ resources/js/components/control-asistencia/asistencia-nuevo.vue
@@ -336,7 +336,10 @@ export default {
   async agregarDetalle() {
     // Verificar si el operador ya está en la lista de detalles
     const existeOperador = this.detalles.some(detalle => detalle.operador.id === this.operador_selected.id);
-    const existeOperadorAyudante = this.detalles.some(detalle => detalle.operador_selected.id === this.operador_ayudante.id);
+    const existeOperadorAyudante = this.operador_selected && this.operador_ayudante
+    ? this.operador_selected.id === this.operador_ayudante.id
+    : false;
+
     // Si el operador ya está en la lista, mostrar un toastr.error
     if (existeOperador) {
       toastr.error('Operador ya seleccionado');
