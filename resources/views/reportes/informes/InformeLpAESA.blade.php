a4690cf7d7f0a82083c46c6d782aa1e5c1fc042f
SergioWeb98 <webser98quinteros@gmail.com>
2025-01-06 11:56:36 -0300

    Update InformeLpAESA.blade.php

--- resources/views/reportes/informes/InformeLpAESA.blade.php
+++ resources/views/reportes/informes/InformeLpAESA.blade.php
@@ -480,7 +480,7 @@ footer{
                             <td id="bordernone">
                                 <p>Firma:
                                     @if($firma)
-                                        <img src="{{ public_path($firma) }}" alt="" style="height:40px;width: 28mm;">
+                                        <img src="{{ public_path($firma) }}" alt="" style="height:40px;width: 18mm;">
                                     @endif
                                 </p>
                             </td>
