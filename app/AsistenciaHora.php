c5b247ce6c21955ebafb62ee677c204c7f52c199
SergioWeb98 <webser98quinteros@gmail.com>
2025-12-01 15:54:55 -0300

    Update AsistenciaHora.php

--- app/AsistenciaHora.php
+++ app/AsistenciaHora.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
 use App\Frentes;
 
 class AsistenciaHora extends Model
@@ -20,12 +19,4 @@ class AsistenciaHora extends Model
     {
         return $this->hasMany(AsistenciaDetalle::class, 'asistencia_horas_id');
     }
-
-    // Formateo de la fecha
-    public function getFechaFormattedAttribute()
-    {
-        return $this->fecha->format('d/m/Y'); // Cambia el formato según tus necesidades
-    }
-
-    protected $dates = ['fecha'];
 }
