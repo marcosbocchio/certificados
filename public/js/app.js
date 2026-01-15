3df5e2e0ea9e8d33662d1eed2375ad8a2119ec44
SergioWeb98 <webser98quinteros@gmail.com>
2026-01-15 14:24:13 -0300

    temp_cambio_chapa

--- public/js/app.js
+++ public/js/app.js
@@ -23856,7 +23856,7 @@ function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input ==
       axios.defaults.baseURL = this.url;
       var foco = this.interno_fuente ? this.interno_fuente.foco : this.interno_equipo.foco;
       foco = foco ? foco : 0;
-      if (this.tecnica.codigo == 'CHAPA') {
+      if (this.tecnica.codigo == 'SWE/SWV') {
         if (this.tecnica && this.medida) {
           this.$store.commit('loading', true);
           var urlRegistros = 'tecnica_distancias/tecnica/' + this.tecnica.id + '/medida/' + this.medida.codigo + '?api_token=' + Laravel.user.api_token;
@@ -25851,7 +25851,7 @@ function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input ==
       axios.defaults.baseURL = this.url;
       var foco = this.interno_fuente ? this.interno_fuente.foco : this.interno_equipo.foco;
       foco = foco ? foco : 0;
-      if (this.tecnica.codigo == 'CHAPA') {
+      if (this.tecnica.codigo == 'SWE/SWV') {
         if (this.tecnica && this.medida) {
           this.$store.commit('loading', true);
           var urlRegistros = 'tecnica_distancias/tecnica/' + this.tecnica.id + '/medida/' + this.medida.codigo + '?api_token=' + Laravel.user.api_token;
--- resources/js/components/informes/informe-ri.vue
+++ resources/js/components/informes/informe-ri.vue
@@ -1858,7 +1858,7 @@ export default {
             axios.defaults.baseURL = this.url;
             let foco = (this.interno_fuente) ? this.interno_fuente.foco : this.interno_equipo.foco;
             foco = foco ? foco : 0;
-            if (this.tecnica.codigo == 'CHAPA') {
+            if (this.tecnica.codigo == 'SWE/SWV') {
                 if (this.tecnica && this.medida) {
                     this.$store.commit('loading', true);
                     var urlRegistros = 'tecnica_distancias/tecnica/' + this.tecnica.id + '/medida/' + this.medida.codigo + '?api_token=' + Laravel.user.api_token;
