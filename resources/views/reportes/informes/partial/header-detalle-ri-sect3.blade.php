b20a5b107131d42028fb174444a15559ef738257
SergioWeb98 <webser98quinteros@gmail.com>
2026-09-11 11:56:01 -0300

    prefijo informes - aesa

--- resources/views/reportes/informes/partial/header-detalle-ri-sect3.blade.php
+++ resources/views/reportes/informes/partial/header-detalle-ri-sect3.blade.php
@@ -4,7 +4,7 @@
             <td width= "44mm" height="4mm" id="left" style="padding-left:1px;">NUMERO DE ENSAYO: </td>
             <td width="60.7mm" height="4mm">
                 @if($contratista && $contratista->formato_especial !== null)
-                    {{$contratista->formato_especial}}-{{$nroAESA}}
+                    RT-{{$contratista->formato_especial}}-{{$nroAESA}}
                 @elseif($planta !== null)
                     RT-{{$planta->codigo}}-ENOD-{{$nroAESA}}
                 @else
